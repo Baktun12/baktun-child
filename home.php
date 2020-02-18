dbfe76db960f5002d53e45c24aa81d0e26b049bb
danman54 <danielibarra@daniel.codes>
2020-02-18 14:50:41 -0800

    add a home.php page

---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copied form default index file
+ * Home page markup 
+ */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+
+<div style="position: fixed; z-index: -99; width: 100%; height: 100%">
+    <video loop="loop" autoplay="" playsinline="" muted="" id="mejs_24989800046482813_html5" preload="none" src="https://www.youtube.com/watch?v=1gJyppALe_g" style="margin: 0px; width: 1425px; height: 801.562px;">
+        <source type="video/mp4" src="https://www.youtube.com/watch?v=1gJyppALe_g">
+    </video>
+</div>
+
+</main><!-- #site-content -->
+<?php	
+get_footer();
+?>
