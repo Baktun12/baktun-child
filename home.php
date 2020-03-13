943444a81b3b8e533661b7ab2c0a01c04f3f1b90
danman54 <danielibarra@daniel.codes>
2020-03-13 03:02:31 -0700

    add grid functionality to home page

--- home.php
+++ home.php
@@ -16,8 +16,10 @@ get_header();
     <div class="swiper-wrapper">
         <!-- Slides -->
         <div class="home swiper-slide">
-        <h4><?php _e('Teatro for the People','baktun-text'); ?></h4>
-        <span class="sub-heading"><?php _e('Exploring the art of teatro as a tool for social change in East Salinas Ca. and beyond.','baktun-text'); ?></span>
+        <h4><?php _e('For our Community. With Love.','baktun-text'); ?></h4>
+        <span class="heart-emoji">❤️</span>
+        <span class="sub-heading"><?php _e('Civic Action Teatro in East Salinas California, and beyond.','baktun-text'); ?></span>
+        <div class="grid"> 
         <?php
           // The Query
           $args = array(
@@ -26,27 +28,70 @@ get_header();
             )
           );
           $the_query = new WP_Query( $args );
-            
           // The Loop
           if ( $the_query->have_posts() ) {
-              echo '<ul class="info-cards">';
+            $post_count = $the_query->found_posts;
+            $half_count = $post_count/2;
+            $loop_count = 0;
+              echo '<div class="column col-1">';
+              $ids = array();
               while ( $the_query->have_posts() ) {
+                  if ( $loop_count > $half_count ) break;
                   $the_query->the_post();
-                  echo '<li>' . get_the_title() . ' ' . get_the_content() . '</li>';
+                  $ids[] = get_the_ID();
+                  echo '<div class="box">';
+                  echo '<h3 class="info-card-heading">' ;
+                  echo get_the_title() . ' </h3>' . get_the_content() ;
                   the_post_thumbnail();
-                  echo '<li>' . tribe_get_start_date() . '</li>';
+                  echo tribe_get_start_date() . '</div>';
+                  $loop_count++;
               }
-              echo '</ul>';
+              echo '</div>';
           } else {
 
               _e('No Posts found.', 'baktun-text');
           }
           /* Restore original Post Data */
           wp_reset_postdata();
+
+          /*  second loop */
+          $args = array(
+            'post_type' => array(
+              'tribe_events', 'post',
+            ),
+            'post__not_in' => $ids
+          );
+          $the_query = new WP_Query( $args );
+          // The Loop
+          if ( $the_query->have_posts() ) {
+              echo '<div class="column col-2">';
+              while ( $the_query->have_posts() ) {
+                  $the_query->the_post();
+                  echo '<div class="box">';
+                  echo '<h3 class="info-card-heading">' ;
+                  echo get_the_title() . ' </h3>' .  get_the_content() ;
+                  the_post_thumbnail();
+                  echo tribe_get_start_date() . '</div>';
+              }
+              echo '</div>';
+          } else {
+
+              _e('No Posts found.', 'baktun-text');
+          }
+          /* Restore original Post Data */
+          wp_reset_postdata();
+
         ?>
-        </div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
+
+
+        </div> <!-- end grid -->
+
+
+        </div> <!-- end Page 1 -->
+        <div class="swiper-slide"> step 2
+        </div> <!-- end Page 2 -->
+        <div class="swiper-slide"> step 3
+        </div> <!-- end Page 3 -->
 
     </div>
 </div>
