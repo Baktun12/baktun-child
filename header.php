964aee60be9e9a977e20755e0ebc6a3dbc0e0c6a
danman54 <danielibarra@daniel.codes>
2020-03-29 21:27:58 -0700

    change menu button text

--- header.php
+++ header.php
@@ -83,7 +83,7 @@
 							<span class="toggle-icon">
 								<?php twentytwenty_the_theme_svg( 'ellipsis' ); ?>
 							</span>
-							<span class="toggle-text"><?php _e( 'Menu', 'twentytwenty' ); ?></span>
+							<span class="toggle-text"><?php _e( 'Español', 'baktun-text' ); ?></span>
 						</span>
 					</button><!-- .nav-toggle -->
 
